299cc80c300d4d21c8e7e8bf31a855f3064d91e7
sshekhar563 <shekharsiddhant93@gmail.com>
2025-12-30 14:34:20 +0530

    feat: Add HouseApplication.java for Spring Boot Basics #115

---
+++ src/main/java/com/opencode/alumxbackend/basics/sshekhar563/house/HouseApplication.java
@@ -0,0 +1,12 @@
+package com.opencode.alumxbackend.basics.sshekhar563.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseApplication.class, args);
+    }
+}
